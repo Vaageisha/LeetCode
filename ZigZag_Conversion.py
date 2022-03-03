d6e75e13438b25cf9d00a094408ec5924562324f
Vaageisha <99316389+Vaageisha@users.noreply.github.com>
2022-03-03 08:03:16 +0530

    Add files via upload

---
+++ ZigZag_Conversion.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        
+        l = len(s)
+
+
+        arr=["" for x in range(l)]
+        row = 0
+        
+        for i in range(l):
+
+            arr[row] += s[i]
+
+            if row == numRows - 1:
+                down = False
+
+            elif row == 0:
+                down = True
+
+            if down:
+                row += 1
+            else:
+                row -= 1
+
+        # Print concatenation
+        # of all rows
+        final = ""
+        for i in range(l):
+            final += arr[i]
+        return final
